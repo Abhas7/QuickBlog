596e8edcfb3f71542a5ab33dc189b00d9c7f4464
abhassomkuwar <abhassomkuwar7@gmail.com>
2026-02-02 22:37:09 +0530

    done

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import { Component } from 'react';
 
 export const adminLogin = async (req, res) => {
     try {
@@ -13,3 +14,33 @@ export const adminLogin = async (req, res) => {
         res.status(500).json({success: false, message: error.message})
     }
 }
+
+
+export const getAllComments = async (req,res) =>{
+    try {
+        const comments = await Comment.find({}).populate('blogId').sort({createdAt:-1})
+        res.json({success: true, comments})
+        } catch (error) {
+            res.json({success: false, message: error.message})
+        
+    }
+}
+
+export const getDashboard = async (req,res) =>{
+    try {
+        const recentBlogs = await blogs.find({}).sort({createdAt: -1}).limit(15);
+        const blogs = await blogs.countDocuments();
+        
+        const comments = await Component.countDocuments();
+        const drafts = await blogs.countDocuments({isPublished: false})
+
+        const getDashboard = {
+            blogs, comments, drafts, recentBlogs
+        } 
+        res.json({success: true, comments})
+        
+    } catch (error) {
+        res.json({success: false, message: error.message})
+        
+    }
+} 
